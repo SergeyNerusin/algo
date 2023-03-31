c957884ec9755e0ea51bbc47e19e0ab5a963cd49
Nerusin Sergey <s.nerusin@yandex.ru>
2023-03-31 18:48:59 +0300

    jest test reverse string function for component string

---
+++ src/components/string/__test__/string.test.js
@@ -0,0 +1,19 @@
+import { reverseStringForTest } from '../secondary-func';
+
+describe('testing the string unfolding algorithm', () => {
+  test('reverse string with even number of characters', () => {
+    expect(reverseStringForTest('abcd')).toEqual(['d', 'c', 'b', 'a']);
+  });
+
+  test('reverse string with odd number of characters', () => {
+    expect(reverseStringForTest('abc')).toEqual(['c', 'b', 'a']);
+  });
+
+  test('function works if the string consists of one character', () => {
+    expect(reverseStringForTest('a')).toEqual(['a']);
+  });
+
+  test('function works if string is empty character', () => {
+    expect(reverseStringForTest('')).toEqual([]);
+  });
+});
