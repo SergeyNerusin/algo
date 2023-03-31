f9a8bc94cc76d3b0111ee1df1d76ee2f894ddcec
Nerusin Sergey <s.nerusin@yandex.ru>
2023-03-31 18:48:59 +0300

    jest test reverse string function for component string

--- src/components/string/secondary-func.ts
+++ src/components/string/secondary-func.ts
@@ -1,16 +1,29 @@
-enum ElementStates {
-  Default = "default",
-  Changing = "changing",
-  Modified = "modified",
-}
+import { ElementStates } from '../../types/element-states';
 
-export const stateCircle = (index: number, currIndex: number, arr: Array<string | number>) => {
-	let arrLength = arr.length - 1;
-	if (currIndex < index || currIndex > arrLength - index) {
-		return ElementStates.Modified;
-	}
-	if (currIndex === index || currIndex === arrLength - index) {
-		return ElementStates.Changing;
-	}
-	return ElementStates.Default;
-}
+export const stateCircle = (
+  index: number,
+  currIndex: number,
+  arr: Array<string | number>
+) => {
+  let arrLength = arr.length - 1;
+  if (currIndex < index || currIndex > arrLength - index) {
+    return ElementStates.Modified;
+  }
+  if (currIndex === index || currIndex === arrLength - index) {
+    return ElementStates.Changing;
+  }
+  return ElementStates.Default;
+};
+
+// для unit теста, проверка алгоритма разворота строки
+export const reverseStringForTest = (stroke: string) => {
+  const lettersArr = stroke.split('');
+  let end = lettersArr.length - 1;
+  let mid = Math.floor(lettersArr.length / 2);
+
+  for (let i = 0; i < mid; i++) {
+    [lettersArr[i], lettersArr[end - i]] = [lettersArr[end - i], lettersArr[i]];
+  }
+
+  return lettersArr;
+};
